c50bce070edd89e975ccbfe52feeb42739640846
Takayuki Ochiai <go.ahead.oti@gmail.com>
2020-02-28 18:25:23 +0900

    Adの属性をNullを許容しない形に変更

--- src/main/kotlin/ktlin/prac/Ad.kt
+++ src/main/kotlin/ktlin/prac/Ad.kt
@@ -3,8 +3,8 @@ package ktlin.prac
 data class Ad(
     val id : Int,
     val adgroupId : Int,
-    val title : String?,
-    val landingPageUrl : String?,
-    val imageUrl : String?,
-    val advertisingSubject : String?
+    val title : String,
+    val landingPageUrl : String,
+    val imageUrl : String,
+    val advertisingSubject : String
     )
