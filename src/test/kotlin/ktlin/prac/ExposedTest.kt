af5d9f6ae5f8e80d25f26017cc36f849f3929892
Takayuki Ochiai <go.ahead.oti@gmail.com>
2020-03-08 20:05:50 +0900

    ExposedTestのアサーションをassertjに変更

--- src/test/kotlin/ktlin/prac/ExposedTest.kt
+++ src/test/kotlin/ktlin/prac/ExposedTest.kt
@@ -6,6 +6,7 @@ import ktlin.adapter.dataaccess.Adgroups
 import ktlin.adapter.dataaccess.Ads
 import ktlin.config.Config
 import ktlin.config.Env
+import org.assertj.core.api.SoftAssertions
 import org.jetbrains.exposed.dao.with
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.greaterEq
@@ -13,7 +14,6 @@ import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
-import kotlin.test.assertEquals
 
 class ExposedTest : Spek({
     beforeEachGroup {
@@ -25,13 +25,15 @@ class ExposedTest : Spek({
             config.env(Env.DB_PASSWORD)
         )
     }
+
+    val softly = SoftAssertions()
+
     group("Adgroup DSL") {
         describe("selectAll") {
             it("count total rows") {
                 transaction {
                     addLogger(StdOutSqlLogger)
                     val allAdgroups = Adgroups.selectAll()
-                    assertEquals(100, allAdgroups.count())
                 }
             }
 
@@ -40,7 +42,7 @@ class ExposedTest : Spek({
                     addLogger(StdOutSqlLogger)
                     val allAdgroups = Adgroups.selectAll()
                     val adgroupIds = allAdgroups.map { it[Adgroups.id].value }
-                    assertEquals(100, adgroupIds.size)
+                    softly.assertThat(adgroupIds.size).isEqualTo(100)
                 }
             }
         }
@@ -52,7 +54,7 @@ class ExposedTest : Spek({
                     // SELECT adgroups.id, adgroups.category_id, adgroups.`name`, adgroups.click_price FROM adgroups WHERE adgroups.`name` = 'hoge'
                     val query = Adgroups.select { Adgroups.name eq "hoge" }
                     val adgroupIds = query.map { it[Adgroups.id].value }
-                    assertEquals(0, adgroupIds.size)
+                    softly.assertThat(adgroupIds.size).isEqualTo(0)
                 }
             }
 
@@ -62,7 +64,7 @@ class ExposedTest : Spek({
                     // SELECT adgroups.id, adgroups.category_id, adgroups.`name`, adgroups.click_price FROM adgroups WHERE adgroups.id = 1
                     val query = Adgroups.select { Adgroups.id eq 1.toLong() }
                     val adgroupIds = query.map { it[Adgroups.id].value }
-                    assertEquals(1, adgroupIds.size)
+                    softly.assertThat(adgroupIds.size).isEqualTo(1)
                 }
             }
 
@@ -75,7 +77,7 @@ class ExposedTest : Spek({
                             .select { Adgroups.id eq 1.toLong() }
                             .map { it[Adgroups.id].value }
 
-                    assertEquals(1, adgroupIds.size)
+                    softly.assertThat(adgroupIds.size).isEqualTo(1)
                 }
             }
 
@@ -90,7 +92,7 @@ class ExposedTest : Spek({
                             .select { condition1 and condition2 }
                             .map { it[Adgroups.id].value }
 
-                    assertEquals(0, andAdgroups.size)
+                    softly.assertThat(andAdgroups.size).isEqualTo(0)
 
                     // SELECT adgroups.id, adgroups.`name` FROM adgroups WHERE adgroups.id >= 50 and name = '広告グループ10'
                     val orAdgroups =
@@ -98,7 +100,7 @@ class ExposedTest : Spek({
                             .select { condition1 or condition2 }
                             .map { it[Adgroups.id].value }
 
-                    assertEquals(52, orAdgroups.size)
+                    softly.assertThat(orAdgroups.size).isEqualTo(52)
 
                     // can also write 'and condition'
                     // SELECT adgroups.id, adgroups.category_id, adgroups.`name`, adgroups.click_price FROM adgroups WHERE adgroups.id >= 50 AND adgroups.`name` = '広告グループ10'
@@ -108,7 +110,7 @@ class ExposedTest : Spek({
                             .andWhere { condition2 }
                             .map { it[Adgroups.id].value }
 
-                    assertEquals(0, andAdgroups2.size)
+                    softly.assertThat(andAdgroups2.size).`as`("can also write 'and condition'").isEqualTo(0)
 
                     // can also write 'or condition'
                     // SELECT adgroups.id, adgroups.category_id, adgroups.`name`, adgroups.click_price FROM adgroups WHERE adgroups.id >= 50 OR adgroups.`name` = '広告グループ10'
@@ -118,7 +120,7 @@ class ExposedTest : Spek({
                             .orWhere { condition2 }
                             .map { it[Adgroups.id].value }
 
-                    assertEquals(52, orAdgroups2.size)
+                    softly.assertThat(orAdgroups2.size).`as`("can also write 'or condition'").isEqualTo(52)
                 }
             }
         }
@@ -130,7 +132,7 @@ class ExposedTest : Spek({
                         addLogger(StdOutSqlLogger)
                         // SELECT adgroups.id, adgroups.category_id, adgroups.`name`, adgroups.click_price FROM adgroups
                         val count = AdgroupDAO.count()
-                        assertEquals(100, count)
+                        softly.assertThat(count).isEqualTo(100)
                     }
                 }
                 it("fetch all columns") {
@@ -138,7 +140,7 @@ class ExposedTest : Spek({
                         addLogger(StdOutSqlLogger)
                         // SELECT adgroups.id, adgroups.category_id, adgroups.`name`, adgroups.click_price FROM adgroups
                         val allAdgroups = AdgroupDAO.all().map(AdgroupDAO::toAdgroup)
-                        assertEquals(100, allAdgroups.size)
+                        softly.assertThat(allAdgroups.size).isEqualTo(100)
                     }
                 }
             }
@@ -148,13 +150,14 @@ class ExposedTest : Spek({
                     transaction {
                         addLogger(StdOutSqlLogger)
                         // SELECT adgroups.id, adgroups.`name` FROM adgroups WHERE adgroups.id = 1
-                        val adgroups = AdgroupDAO.find { Adgroups.id eq 1.toLong() }
-                            .map(AdgroupDAO::toAdgroup)
-                        assertEquals(1, adgroups.size)
+                        val adgroups =
+                            AdgroupDAO.find { Adgroups.id eq 1.toLong() }
+                                .map(AdgroupDAO::toAdgroup)
+                        softly.assertThat(adgroups.size).isEqualTo(1)
 
                         // SELECT adgroups.id, adgroups.category_id, adgroups.`name`, adgroups.click_price FROM adgroups WHERE adgroups.id = 1
                         val adgroup = AdgroupDAO.findById(1.toLong())?.toAdgroup()
-                        assertEquals(1.toLong(), adgroup?.id)
+                        softly.assertThat(adgroup?.id).isEqualTo(1.toLong())
                     }
                 }
 
@@ -164,14 +167,15 @@ class ExposedTest : Spek({
                         val condition1 = Adgroups.id greaterEq 50.toLong()
                         val condition2 = Adgroups.name eq "広告グループ10"
                         // SELECT adgroups.id, adgroups.category_id, adgroups.`name`, adgroups.click_price FROM adgroups WHERE adgroups.id >= 50 AND adgroups.`name` = '広告グループ10'
-                        var adgroups = AdgroupDAO.find { condition1 and condition2 }
-                            .map(AdgroupDAO::toAdgroup)
-                        assertEquals(0, adgroups.size)
+                        var adgroups =
+                            AdgroupDAO.find { condition1 and condition2 }
+                                .map(AdgroupDAO::toAdgroup)
+                        softly.assertThat(adgroups.size).isEqualTo(0)
 
                         // SELECT adgroups.id, adgroups.category_id, adgroups.`name`, adgroups.click_price FROM adgroups WHERE adgroups.id >= 50 OR adgroups.`name` = '広告グループ10'
                         adgroups = AdgroupDAO.find { condition1 or condition2 }
                             .map(AdgroupDAO::toAdgroup)
-                        assertEquals(52, adgroups.size)
+                        softly.assertThat(adgroups.size).isEqualTo(52)
                     }
                 }
             }
@@ -186,23 +190,25 @@ class ExposedTest : Spek({
                     val adsQuery = Ads.innerJoin(Adgroups).slice(Ads.columns).selectAll().limit(10)
                     // DSLでjoinとかしたqueryを元にDAO生成する
                     val ads = AdDAO.wrapRows(adsQuery).map(AdDAO::toAd)
-                    assertEquals(10 , ads.size)
-                    assertEquals(1 , ads.first().id)
+
+                    softly.assertThat(ads.size).isEqualTo(10)
+                    softly.assertThat(ads.first().id).isEqualTo(1)
                 }
             }
 
             it("allows you to select with join and expressions") {
                 transaction {
                     addLogger(StdOutSqlLogger)
-                    val adsQuery = Ads.innerJoin(Adgroups).slice(Ads.columns)
-                        .selectAll()
-                        .andWhere { Adgroups.id greaterEq  90.toLong() }
+                    val adsQuery =
+                        Ads.innerJoin(Adgroups).slice(Ads.columns)
+                            .selectAll()
+                            .andWhere { Adgroups.id greaterEq 90.toLong() }
                     // DSLでjoinとかしたqueryを元にDAO生成する
                     // with(AdDAO::adgroup)でAdgroupDAOをeager loadするようにしている
                     // SQL: SELECT ads.id, ads.adgroup_id, ads.title, ads.landing_page_url, ads.image_url, ads.advertising_subject FROM ads INNER JOIN adgroups ON adgroups.id = ads.adgroup_id WHERE adgroups.id >= 90
                     // SQL: SELECT adgroups.id, adgroups.category_id, adgroups.`name`, adgroups.click_price FROM adgroups WHERE adgroups.id IN (100, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99)
                     val ads = AdDAO.wrapRows(adsQuery).with(AdDAO::adgroup).map(AdDAO::toAd)
-                    assertEquals(1100 , ads.size)
+                    softly.assertThat(ads.size).isEqualTo(1100)
                 }
             }
         }
